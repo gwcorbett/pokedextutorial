605ab9bec6d5beb5d478d130be2e869f5aacde93
Greg <gwcorbett@gmail.com>
2022-10-24 18:30:47 -0600

    End of Vid

--- src/components/PokedexCard.jsx
+++ src/components/PokedexCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 
 const PokedexCard = ({pokemon, index, setTeam}) => {
 
     const addToTeam = () =>{
-        setTeam((currentVal) =>{
-            if(currentVal.length === 6) return currentVal
-            let newState = [...currentVal, pokemon.id]
-            return newState
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.id}`)
+            .then((res) =>{
+                setTeam((currentVal) =>{
+                    if(currentVal.length === 6) return currentVal
+                    let newState = [...currentVal, res.data]
+                    return newState
         })
-    }
+    })}
   return (
     <div className='pokedex-card'>
         <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} className='pokemon-img' />
--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,37 +1,16 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React from 'react'
 
 const TeamSection = ({team}) =>{
-    const [teamDetails, setTeamDetails] = useState([])
 
-    console.log(teamDetails)
-
-    useEffect(() =>{
-        // let newDetails = []
-    //     team
-    //     .forEach((id) =>{
-    //         axios
-    //         .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    //         .then((res) =>{
-    //             newDetails.push(res.data)
-    //         })
-    //     })
-    //     .finally(() =>{
-    //         setTeamDetails(newDetails)
-    //     })
-    // },[team])
-    let newDetails = team.map
+    let display = team.map((pokemon, index) =>{
+        return <h3>{pokemon.name}</h3>
+    })
 
     return (
         <section className='team-section'>
             <h2>My Team</h2>
             <div className='team-container'>
-                <h3>Hello</h3>
-                <h3>Hello</h3>
-                <h3>Hello</h3>
-                <h3>Hello</h3>
-                <h3>Hello</h3>
-                <h3>Hello</h3>
+                {display}
             </div>
         </section>
     )
